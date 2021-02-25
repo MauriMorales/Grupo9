490cbd77e84c1fb341713f47bbde663572969b17
MauriMorales <mauridanielmorales@gmail.com>
2021-02-25 20:34:44 -0300

    hola lucas

--- Lucas.js
+++ Lucas.js
@@ -1,1 +1,2 @@
 console.log("Lucas")
+//Hola Lucas
--- Roberto.js
+++ Roberto.js
@@ -1,8 +1,5 @@
 console.log("Roberto")
-<<<<<<< HEAD
 //Hola Roberto
-=======
 
 
 // ¿Como estas?
->>>>>>> a7789745effb1b1739fdb24fc316f42d6c9712f0
